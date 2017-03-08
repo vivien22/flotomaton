6fb97e9e8d4de78f3fc0edc1b0ea610f1635998d
Vivien Jouan <bibien22@hotmail.com>
2017-03-08 14:33:23 +0100

    [gpio] update gpio library.

--- gpio.py
+++ gpio.py
@@ -3,53 +3,47 @@
 import time
 import RPi.GPIO as GPIO
 
+# Pinout 
+button_1 = 3
+button_2 = 5
+button_3 = 7
 
 # handle the button event
 def buttonEventHandler (pin):
-    print "handling button event"
+    if   pin == button_1:
+        print("handling button 1 event")
+    elif pin == button_2:
+        print("handling button 2 event")
+    elif pin == button_3:
+        print("handling button 3 event")
+    else:
+        print("unhandled gpio event")
 
-    # turn the green LED on
-    GPIO.output(25,True)
+class init(object):
+  
+    def __init__(self):
+        # tell the GPIO module that we want to use 
+        # the chip's pin numbering scheme
+        GPIO.setmode(GPIO.BCM)
 
-    time.sleep(1)
+        # setup buttons as an inputs
+        GPIO.setup(button_1,GPIO.IN)
+        GPIO.setup(button_2,GPIO.IN)
+        GPIO.setup(button_3,GPIO.IN)
 
-    # turn the green LED off
-    GPIO.output(25,False)
-
-
-
-# main function
-def main():
-
-    # tell the GPIO module that we want to use 
-    # the chip's pin numbering scheme
-    GPIO.setmode(GPIO.BCM)
-
-    # setup pin 23 as an input
-    # and set up pins 24 and 25 as outputs
-    GPIO.setup(23,GPIO.IN)
-    GPIO.setup(24,GPIO.OUT)
-    GPIO.setup(25,GPIO.OUT)
-
-    # tell the GPIO library to look out for an 
-    # event on pin 23 and deal with it by calling 
-    # the buttonEventHandler function
-    GPIO.add_event_detect(23,GPIO.FALLING)
-    GPIO.add_event_callback(23,buttonEventHandler)
-
-    # turn off both LEDs
-    GPIO.output(25,False)
-    GPIO.output(24,True)
-
-    # make the red LED flash
-    while True:
-        GPIO.output(24,True)
-        time.sleep(1)
-        GPIO.output(24,False)
-        time.sleep(1)
+        # tell the GPIO library to look out for an 
+        # event on button's pins and deal with it by calling 
+        # the buttonEventHandler function
+        GPIO.add_event_detect  (button_1,GPIO.FALLING)
+        GPIO.add_event_callback(button_1,buttonEventHandler)
+        GPIO.add_event_detect  (button_2,GPIO.FALLING)
+        GPIO.add_event_callback(button_2,buttonEventHandler)
+        GPIO.add_event_detect  (button_3,GPIO.FALLING)
+        GPIO.add_event_callback(button_3,buttonEventHandler)
 
 
-    GPIO.cleanup()
+    def close(self):
+        GPIO.cleanup()
 
 if __name__=="__main__":
     main()
