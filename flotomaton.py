74b96bf33d63c9fdbbbc5023b86f49704ca6d238
Vivien Jouan <bibien22@hotmail.com>
2017-03-08 13:03:51 +0100

    [main] Add flotomaton.py file (final entry point)

--- flotomaton.py
+++ flotomaton.py
@@ -1,5 +1,3 @@
-# http://www.projetsdiy.fr/picamera-version-1-9-piloter-en-python-la-camera-du-raspberry-pi/#.WKsablXhAdU
-
 import io, time, os, sys, picamera, pygame
  
 # Display warning for deprecated Picamera functions (since v1.8) / affiche alerte si une fonction depreciee est utilisee 
@@ -11,12 +9,23 @@ vid_taken = 0
  
 # Init pygame and screen / initialise pygame et ecran
 pygame.init()
-res = pygame.display.list_modes()   # return the resolution of your monitor / resolution du moniteur
+res = pygame.display.list_modes()   # return the resolution of your monitor 
 width, height = res[0]              # In case of trouble, set manually the resolution with: width, height = 1650, 1050
 print "Screen resolution :", width, "x", height
 screen = pygame.display.set_mode([width, height])
 pygame.display.toggle_fullscreen()
 pygame.mouse.set_visible = False
+
+#Font 3,2,1 for countdown
+font = pygame.font.SysFont("arial", 256)
+font_colour = (127, 127, 127)
+text_3 = font.render("3", True, font_colour)
+text_2 = font.render("2", True, font_colour)
+text_1 = font.render("1", True, font_colour)
+
+# Load, resize & display background
+fond = pygame.image.load("fond.png").convert()
+fond = pygame.transform.scale(fond, (width, height))
  
 # Picamera object / objet Picamera
 camera = picamera.PiCamera()
@@ -24,6 +33,20 @@ camera.framerate = float(24)
  
 # Define functions / fonctions
 def take_pic():
+    
+    screen.fill(pygame.Color("black")) # erases the entire screen surface
+    screen.blit(text_2, ((width - text_2.get_width()) // 2, (height - text_2.get_height()) // 2))
+    pygame.display.flip()
+    pygame.time.wait(1000)
+
+    screen.fill(pygame.Color("black")) # erases the entire screen surface
+    screen.blit(text_1, ((width - text_1.get_width()) // 2, (height - text_1.get_height()) // 2))
+    pygame.display.flip()
+    pygame.time.wait(1000)
+
+    screen.fill(pygame.Color("black")) # erases the entire screen surface
+    pygame.display.flip()
+    
     global pics_taken
     pics_taken += 1
     camera.capture('image_' + str(pics_taken) + '.jpg')
@@ -42,8 +65,13 @@ def quit_app():
     print "You've taken", pics_taken, " pictures ", vid_taken, " videos. Don't forget to back them up (or they'll be overwritten next time)"
     sys.exit(0)
  
-#Start camera preview / Demarre affichage en direct
+# Start camera preview / Demarre affichage en direct
 camera.start_preview()
+
+# Display selection menu
+camera.preview.alpha = 128
+screen.blit(fond, (0,0))
+pygame.display.flip()
 
 while(True):
   pygame.display.update()
@@ -53,7 +81,5 @@ while(True):
           quit_app()
         elif event.key == pygame.K_SPACE:
           take_pic()
-        elif event.key == pygame.K_RETURN:
-          take_video()
         elif event.key == pygame.K_TAB:
            camera.start_preview()
