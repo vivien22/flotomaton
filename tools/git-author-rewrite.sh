ca4780039a8181adc41c7a93c05f147ea624a272
Vivien Jouan <bibien22@hotmail.com>
2017-03-09 14:03:06 +0100

    [tool] add rewriter script to replace wrong mail address in git history.

---
+++ tools/git-author-rewrite.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="vivien.jouan@itron.com"
+CORRECT_NAME="Vivien Jouan"
+CORRECT_EMAIL="bibien22@hotmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
