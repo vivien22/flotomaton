41ae9129367cb291b9b33ed7db7312b7e96ba369
Vivien Jouan <bibien22@hotmail.com>
2017-03-10 15:23:04 +0100

    [gphoto] create gphoto wrapper.

--- src/gphoto.py
+++ src/gphoto.py
@@ -1,4 +1,47 @@
+import logging
+import os
 import gphoto2 as gp
 
-# TODO
+class gphoto(object):
+  
+    def __init__(self):
+
+        logging.basicConfig(format='%(levelname)s: %(name)s: %(message)s', level=logging.WARNING)
+        gp.check_result(gp.use_python_logging())
+        self.context = gp.gp_context_new()
+        self.camera  = gp.check_result(gp.gp_camera_new())
+        gp.check_result(gp.gp_camera_init(self.camera, self.context))
+
+    def close(self):
+        gp.gp_camera_exit(self.camera, self.context)
+
+    def camera_capture(self):
+        return gp.check_result(gp.gp_camera_capture(self.camera, 
+                                                    gp.GP_CAPTURE_IMAGE, 
+                                                    self.context))
+
+    def camera_file_get_and_save(self, file_path, target):
+        camera_file = gp.check_result(gp.gp_camera_file_get(self.camera, 
+                                                            file_path.folder, 
+                                                            file_path.name, 
+                                                            gp.GP_FILE_TYPE_NORMAL, 
+                                                            self.context))
+
+        gp.check_result(gp.gp_file_save(camera_file, target))
+
+        return camera_file
+
+    def capture_single_image(self, dst_file_path):
+        print('Capturing image')
+        file_path = camera_capture()
+        print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
+        target = os.path.join('/tmp', file_path.name)
+        print('Copying image to', target)
+        return camera_file_get(file_path, target)
+
+
+if __name__=="__main__":
+    gplib = gphoto()
+    gplib.capture_single_image('/tmp')
+    gplib.close()
 
--- tools/gphoto-installer.sh
+++ tools/gphoto-installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # gphoto2 & libgphoto2
-sudo apt-get install gphoto2
+sudo apt-get install gphoto2 libjpeg9-dev
 wget https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh && chmod +x gphoto2-updater.sh && sudo ./gphoto2-updater.sh
 rm gphoto2-updater.sh
 
