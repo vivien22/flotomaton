91d46710516bc08fcec6ed6ca4e2a5d47b6b1970
Vivien Jouan <bibien22@hotmail.com>
2017-04-04 13:20:02 +0200

    [gpio] set gpio input with pulldown

--- src/gpio.py
+++ src/gpio.py
@@ -1,4 +1,5 @@
 # http://raspberrywebserver.com/gpio/using-interrupt-driven-gpio.html
+# http://razzpisampler.oreilly.com/ch07.html
 
 import time
 import RPi.GPIO as GPIO
@@ -30,21 +31,21 @@ class init(object):
         GPIO.setmode(GPIO.BOARD)
 
         # setup buttons as an inputs
-        GPIO.setup(button_1,GPIO.IN)
-        GPIO.setup(button_2,GPIO.IN)
-        GPIO.setup(button_3,GPIO.IN)
-        GPIO.setup(button_4,GPIO.IN)
+        GPIO.setup(button_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(button_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(button_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(button_4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
         # tell the GPIO library to look out for an 
         # event on button's pins and deal with it by calling 
         # the buttonEventHandler function
-        GPIO.add_event_detect  (button_1,GPIO.RISING, bouncetime=500)
+        GPIO.add_event_detect  (button_1,GPIO.FALLING, bouncetime=500)
         GPIO.add_event_callback(button_1,buttonEventHandler)
-        GPIO.add_event_detect  (button_2,GPIO.RISING, bouncetime=500)
+        GPIO.add_event_detect  (button_2,GPIO.FALLING, bouncetime=500)
         GPIO.add_event_callback(button_2,buttonEventHandler)
-        GPIO.add_event_detect  (button_3,GPIO.RISING, bouncetime=500)
+        GPIO.add_event_detect  (button_3,GPIO.FALLING, bouncetime=500)
         GPIO.add_event_callback(button_3,buttonEventHandler)
-        GPIO.add_event_detect  (button_4,GPIO.RISING, bouncetime=500)
+        GPIO.add_event_detect  (button_4,GPIO.FALLING, bouncetime=500)
         GPIO.add_event_callback(button_4,buttonEventHandler)
 
     def close(self):
